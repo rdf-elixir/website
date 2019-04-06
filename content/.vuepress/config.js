c63285b9f18431e46f2d9f9d38f7c1ff8a277d19
Marcel Otto <marcelotto@gmx.de>
2019-04-07 00:14:18 +0200

    Add favicons

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -1,6 +1,22 @@
 module.exports = ctx => ({
   title: 'RDF on Elixir',
   description: 'Implementation of the Linked Data and Semantic Standards for Elixir',
+  head: [
+    ['link', { rel: 'manifest', href: '/icons/manifest.json' }],
+    // For new browsers multisize ico
+    ['link', { rel: 'icon', type: 'image/x-icon', sizes: '16x16 32x32', href: '/icons/favicon.ico' }],
+    // Chrome for Android
+    ['link', { rel: 'icon', sizes: '192x192', href: '/icons/favicon-192.png' }],
+    // For iPhone 6+ downscaled for other devices
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/favicon-180-precomposed.png' }],
+    // For IE10 Metro
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/favicon-144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#FFFFFF' }],
+
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+  ],
   themeConfig: {
     repo: 'marcelotto/rdf-elixir-website',
     docsDir: 'content',
